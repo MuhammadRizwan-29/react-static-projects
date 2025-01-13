d2f08071a503bcff30cb96b79e7ac0d6574dd951
MuhammadRizwan-29 <engrrizwanjamali@gmail.com>
2025-01-13 21:44:35 +0500

    Code added UndoLater

--- react-tabs/src/components/TabContent/TabContent.jsx
+++ react-tabs/src/components/TabContent/TabContent.jsx
@@ -6,9 +6,15 @@ export default function TabContent({ item }) {
   function handleInc() {
     setLikes(likes + 1);
   }
+  function handleTrippleInc() {
+    setLikes((likes) => likes + 3);
+  }
   function handleUndo() {
     setShowDetails(true);
     setLikes(0);
+  }
+  function handleUndoLater() {
+    setTimeout(handleUndo, 2000);
   }
   return (
     <div className="tab-content">
@@ -22,13 +28,13 @@ export default function TabContent({ item }) {
         <div className="hearts-counter">
           <span>{likes} ❤️</span>
           <button onClick={handleInc}>+</button>
-          <button>+++</button>
+          <button onClick={handleTrippleInc}>+++</button>
         </div>
       </div>
 
       <div className="tab-undo">
-        <button onClick={() => handleUndo}>Undo</button>
-        <button>Undo in 2s</button>
+        <button onClick={handleUndo}>Undo</button>
+        <button onClick={handleUndoLater}>Undo in 2s</button>
       </div>
     </div>
   );
